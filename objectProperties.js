9e2ddc9c8e689d2a35db6c89e4c9c0d260ebe4b6
Jethet <henriette.hettinga@gmail.com>
2019-10-25 16:44:21 +0200

    Example of accessing property values through variables

---
+++ objectProperties.js
@@ -0,0 +1,11 @@
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber = 16;       // This stores property 16 in var playerNumber //
+var player = testObj[playerNumber];   // This accesses the value of property 16 through variable playerNumber //
