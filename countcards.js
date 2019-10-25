46dcaa1fd65949026aa79990df173db338f57866
Jethet <henriette.hettinga@gmail.com>
2019-10-25 16:00:09 +0200

    Card game

---
+++ countcards.js
@@ -0,0 +1,20 @@
+var count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+    count =+ 1;
+    console.log(count + " Bet");
+    break;
+  }
+
+  return "Change Me";
+}
+
+cc(5)
+cc(4)
