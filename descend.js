6fa3063d77ab60d313da2d79169c53042c8432c6
Jethet <henriette.hettinga@gmail.com>
2019-10-28 09:44:18 +0100

    JS recursive functions

--- descend.js
+++ descend.js
@@ -2,12 +2,3 @@
 and return it with its digits in descending order. Essentially, rearrange the digits to
 create the highest possible number.
 Examples: input: 21445    output: 54421*/
-
-function foo(i) {
-  if (i < 0)
-    return;
-  console.log('begin: ' + i);
-  foo(i - 1);
-  console.log('end: ' + i);
-}
-foo(3);
---
+++ recursive.js
@@ -0,0 +1,25 @@
+// Recursive function: start with the exit condition and return
+// add the 'do something' after that
+// The function can call itself: see foo() and multiply()
+
+function foo(i) {
+  if (i <= 0)
+    return;
+  console.log('begin: ' + i);
+  foo(i - 1);
+  console.log('end: ' + i);
+}
+
+
+
+function multiply(arr, n) {
+   if (n <= 0) {
+     return arr[0];
+   } else {
+     return multiply(arr, n - 1) * arr[n];
+   }
+  console.log(arr);
+ }
+
+foo(3);
+multiply([1, 2, 3], 6);
