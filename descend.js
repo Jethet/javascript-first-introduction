124a35521bb6f85b9b82b8fa9831da2463992a59
Jethet <henriette.hettinga@gmail.com>
2019-10-28 10:02:55 +0100

    Number descend

--- descend.js
+++ descend.js
@@ -2,3 +2,14 @@
 and return it with its digits in descending order. Essentially, rearrange the digits to
 create the highest possible number.
 Examples: input: 21445    output: 54421*/
+
+function descend(number) {
+  for (let i = 0; i <= number[0]; i++) {
+    if number[0] < i {
+      number.shift() && number.push();
+      i++;
+    } else {
+      return number;
+    }
+  }
+}
