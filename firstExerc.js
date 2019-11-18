43a237d435a8a727a381c95ddd213207bce75eff
Jethet <henriette.hettinga@gmail.com>
2019-11-18 17:45:55 +0100

    String exercises

--- firstExerc.js
+++ firstExerc.js
@@ -6,20 +6,30 @@ const str1 = "Web Dev is amazing!";
     // - "web",
     // - "ing",
     // - "Web"
+console.log(str1.includes("development"));
+console.log(str1.includes("Development"));
+console.log(str1.includes("web"));
+console.log(str1.includes("ing"));
+console.log(str1.includes("Web"));
+
 
 //Concatenate the following strings:
 let str2 = "Hey!";
 let str3 = "How are you?";
 // end result: "Hey! How are you?"
+console.log(str2 + " " + str3);
 
 let str4 = 10;
 let str5 = 99;
 // end result: 1099
+console.log(str4 * 100 + 99);
 
 //Take out half of the sentence:
 let sentence = "This is one meaningless sentence which needs to be cut in half.";
 // end result: This is one meaningless sentence
-
+console.log(sentence.length);
+console.log(sentence.slice(0,32));
+console.log(sentence.slice(32, 63));
 /* Capitalize:
 a) the whole name of the city where you are now
 b) the first letter of the city
