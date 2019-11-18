88c63219116b8322d56239bd02196f6f577f63ee
Jethet <henriette.hettinga@gmail.com>
2019-11-18 16:56:21 +0100

    Test strings and concatenation

---
+++ test_stringtips.js
@@ -0,0 +1,18 @@
+const price = 12.99;
+const statement = `The price of this amazing product is ${price}.`;
+console.log(statement, price);
+
+console.log(statement + " This is amazing");
+
+const campuses = `
+  - Madrid
+  - Barcelona
+  - ...
+  - ...
+  - Lisbon`;
+
+  console.log(`Multiline string and interpolation 😉: ${campuses}`);
+
+let text = "This is the first part.";
+text += " So what?";
+console.log(text);
