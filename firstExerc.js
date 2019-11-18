977d2b7f7b07e9b6dedd40b9e13fdd829fefc32f
Jethet <henriette.hettinga@gmail.com>
2019-11-18 17:34:23 +0100

    Create file for Ironhack string exercises

---
+++ firstExerc.js
@@ -0,0 +1,29 @@
+//Find out if there is a specific word in the string:
+const str1 = "Web Dev is amazing!";
+    // => look for each of these separately:
+    // - "development",
+    // - "Development",
+    // - "web",
+    // - "ing",
+    // - "Web"
+
+//Concatenate the following strings:
+let str2 = "Hey!";
+let str3 = "How are you?";
+// end result: "Hey! How are you?"
+
+let str4 = 10;
+let str5 = 99;
+// end result: 1099
+
+//Take out half of the sentence:
+let sentence = "This is one meaningless sentence which needs to be cut in half.";
+// end result: This is one meaningless sentence
+
+/* Capitalize:
+a) the whole name of the city where you are now
+b) the first letter of the city
+c) capitalize the middle letter if the city has an odd number of letters or
+two middle letters if the city has even number of letters
+(hint: you can use .charAt() and .slice())
+*/
