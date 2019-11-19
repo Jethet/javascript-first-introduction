ec98b72a98870b1f202b008401cc6a678001dad4
Jethet <henriette.hettinga@gmail.com>
2019-11-19 14:37:53 +0100

    Practice switch

---
+++ exSwitch.js
@@ -0,0 +1,15 @@
+let colour = "1";
+
+
+switch (colour) {
+  case "5":
+    console.log("Is the right colour");
+    break;
+  case "8":
+    console.log("Not the right colour");
+    break;
+  case "1":
+    console.log("Not the right colour");
+    break;
+
+}
