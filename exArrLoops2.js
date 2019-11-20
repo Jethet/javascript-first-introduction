f50040c973b76a7c61d1f42af61ca8f2a8a4afca
Jethet <henriette.hettinga@gmail.com>
2019-11-20 12:33:02 +0100

    Practice while loop

--- exArrLoops2.js
+++ exArrLoops2.js
@@ -1,7 +1,15 @@
+
 let newArray = ["one", "two", "three", "four", "five"];
 let counter = 0;
 
 while (counter < newArray.length){
-  console.log(counter);
-
+  console.log(`Counter (===index): ${counter} - element: ${newArray[counter]}`);
+  counter++;
 }
+
+/*
+while (counter < newArray.length){
+  console.log(counter);
+  counter++;
+}
+*/
