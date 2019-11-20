aaacd4ddb2b5ff3a83a5c3b610e8aa19c79a6c12
Jethet <henriette.hettinga@gmail.com>
2019-11-20 12:42:29 +0100

    Testing template literals: OK

--- exArrLoops2.js
+++ exArrLoops2.js
@@ -6,10 +6,25 @@ while (counter < newArray.length){
   console.log(`Counter (===index): ${counter} - element: ${newArray[counter]}`);
   counter++;
 }
+/* Result:
+Counter (===index): 0 - element: one
+Counter (===index): 1 - element: two
+Counter (===index): 2 - element: three
+Counter (===index): 3 - element: four
+Counter (===index): 4 - element: five
+*/
 
-/*
-while (counter < newArray.length){
-  console.log(counter);
-  counter++;
+let myArray = ["one", "two", "three", "four", "five"];
+let newcounter = 0;
+
+while (newcounter < myArray.length){
+  console.log(`These are the counts: ${newcounter}`);
+  newcounter++;
 }
+/* Result:
+These are the counts: 0
+These are the counts: 1
+These are the counts: 2
+These are the counts: 3
+These are the counts: 4
 */
