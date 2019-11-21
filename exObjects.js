cac355a9c646bec6ab947b2f85bf0d94cac2c2b7
Jethet <henriette.hettinga@gmail.com>
2019-11-21 17:31:28 +0100

    Test object syntax

---
+++ exObjects.js
@@ -0,0 +1,13 @@
+let myObject = {
+  key1: "Value 1",
+  key2: "Value 2",
+  key3: "Value3"
+};
+
+console.log(myObject);
+console.log(myObject.key3);
+
+let x = myObject;
+
+console.log(x);
+console.log(x.key1);
