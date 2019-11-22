adea73ef66051b7394098d9c049e82c605e6cbeb
Jethet <henriette.hettinga@gmail.com>
2019-11-22 11:17:03 +0100

    Third exercise objects

---
+++ exObjects3.js
@@ -0,0 +1,23 @@
+let product = {
+    name: "headphones",
+    price: 100
+};
+
+if (product.price > 100){
+  let discount = 10 / 100;
+  let discPrice = product.price - (product.price * discount);
+  product.price = discPrice;
+  product.discount = discount;
+} else {
+  discount = 7 / 100;
+  discPrice = product.price - (product.price * discount);
+  product.price = discPrice;
+  product.discount = discount;
+}
+
+console.log(product.price);
+console.log(product);
+
+if ("discount" in product){
+  console.log(`Already discounted by ${discount}.`);
+}
