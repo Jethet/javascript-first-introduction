af8a749f0740b9de3eb84938b07a0311e26a405e
Jethet <henriette.hettinga@gmail.com>
2019-11-22 15:11:09 +0100

    Add explanation nested objects

---
+++ exNestedData.js
@@ -0,0 +1,13 @@
+const theseObj = [
+    {
+      prop1: "One",
+      prop2: "Two",
+    },
+    {
+      prop1: "Word",
+      prop2: "Sentence"
+    }
+];
+
+console.log(theseObj[0]);
+console.log(theseObj[0].prop1);
