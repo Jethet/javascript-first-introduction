70e770ff84bf5af2ae618aa2846704e1ed49d3a1
Jethet <henriette.hettinga@gmail.com>
2019-11-23 14:22:26 +0100

    Practice functions count

--- exFunctions2.js
+++ exFunctions2.js
@@ -1,4 +1,4 @@
-function arraySum(array){
+/*function arraySum(array){
     let result = 0
     for(let i = 0; i < array.length; i++){
          result += array[i];
@@ -7,3 +7,13 @@ function arraySum(array){
 }
 
 const prices = arraySum([5, 7.99, 9.99, 0.99, 21]);
+
+
+Define a function that will:
+    count from 1 to 100,
+    on numbers divisible with 4 print “Hey”,
+    on numbers divisible with 6 print “There”,
+    on numbers divisible with both 4 and 6 print “Ironhack”,
+    skip numbers divisible with 7,
+    on the number that represents your age add ! (ex. 34!).
+*/
