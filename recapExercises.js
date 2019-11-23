e45b60d3611b8ee31ee19fe754bfe01b90c3512d
Jethet <henriette.hettinga@gmail.com>
2019-11-23 17:12:29 +0100

    Work on typeof exercise

---
+++ recapExercises.js
@@ -0,0 +1,11 @@
+// Define a function, name it however you prefer respecting the good naming conventions,
+// that will do the same as we did earlier - loop through the array and print the type
+// of each element of the array in the console.
+
+const someArr = ["max", 34, true, {name: "sandra", student: true}, 
+["javascript", "mongodb", "react"]];
+
+function printType(array)
+    
+
+printType([someArr]);
