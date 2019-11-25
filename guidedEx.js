2be3f842658f236f859a5ba439953a493eb7bc75
Jethet <henriette.hettinga@gmail.com>
2019-11-25 18:38:00 +0100

    First setup board and player

---
+++ guidedEx.js
@@ -0,0 +1,13 @@
+// Guided example of board play
+
+// board = [ [{},{},{}], [{},{},{}] [{},{},{}] ] from 0 to 2
+
+let board = [[{x:0, y:0}, {x:0, y:1}, {x:0, y:2}], 
+            [{x:1, y:0,}, {x:1, y:1}, {x:1, y:2}], 
+            [{x:2, y:0}, {x:2, y:1}, {x:2, y:2}]]; 
+
+const player = {
+    x:1,
+    y:1
+};
+
