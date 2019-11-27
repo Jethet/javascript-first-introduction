1df02bc6a348b81e4b2d546de004a53a5964a448
Jethet <henriette.hettinga@gmail.com>
2019-11-27 19:27:48 +0100

    Bring direction functions into switch statement function

--- marsRover.js
+++ marsRover.js
@@ -6,6 +6,10 @@
 
 // ======================
 
+let rover = {
+    direction: "N"
+};
+
 function turnLeft(rover){
     console.log("turnLeft was called");
 }
@@ -16,4 +20,22 @@ function turnRight(rover){
 
 function moveForward(rover){
     console.log("moveForward was called");
+}
+
+function turnRover(rover, directions){
+    for (i = 0; i <= direction.length; i++){
+        let direction = directions[i];
+        switch(direction){
+            case "l":
+                turnLeft(rover, direction);
+                break;
+            case "r":
+                turnRight(rover, direction);
+                break;
+            case "f":
+                moveForward(rover, direction);
+                break;
+        }
+        console.log(`Rover facing direction ${direction}`);
+    }
 }
