1e5fe3c3bc55863e1df4c3a1a2fc39c8afb781d7
Jethet <henriette.hettinga@gmail.com>
2019-12-04 17:26:16 +0100

    Correct error turnLeft function

--- marsRover.js
+++ marsRover.js
@@ -6,32 +6,32 @@ let rover = {
 function turnLeft(rover){
     if (rover.direction === "N"){
         rover.direction = "W";
-        console.log(rover.direction); 
+        //console.log(rover.direction); 
     } else if (rover.direction === "W"){
         rover.direction = "S";
-        console.log(rover.direction); 
+        //console.log(rover.direction); 
     } else if (rover.direction === "S"){
         rover.direction = "E";
-        console.log(rover.direction);
-    } else if (rover.direction === "W"){
-            rover.direction = "S";
-            console.log(rover.direction);
+        //console.log(rover.direction);
+    } else if (rover.direction === "E"){
+            rover.direction = "N";
+            //console.log(rover.direction);
     }
 }
 
 function turnRight(rover){
     if (rover.direction === "N"){
         rover.direction = "E";
-        console.log(rover.direction); 
+        //console.log(rover.direction); 
     } else if (rover.direction === "E"){
         rover.direction = "S";
-        console.log(rover.direction); 
+        //console.log(rover.direction); 
     } else if (rover.direction === "S"){
         rover.direction = "W";
-        console.log(rover.direction);
+        //console.log(rover.direction);
     } else if (rover.direction === "W"){
             rover.direction = "N";
-            console.log(rover.direction);
+            //console.log(rover.direction);
     }
 }
 
@@ -55,5 +55,5 @@ function manageRover(rover, directions){
         
     }
 }
-manageRover(rover, "lrrr");
+manageRover(rover, "lrrrllr");
 
