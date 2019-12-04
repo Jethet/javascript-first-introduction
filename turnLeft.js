cb383a136dc386de73326319f46e70689c7e8906
Jethet <henriette.hettinga@gmail.com>
2019-12-04 14:22:40 +0100

    Try turnLeft function

---
+++ turnLeft.js
@@ -0,0 +1,12 @@
+let rover = {
+    direction: "N"
+};
+
+let directions = "";
+
+function turnLeft(rover){
+    if (rover.direction === "N" && directions === "l"){
+            rover.direction === "W";
+            console.log(rover.direction);
+  }   
+}
