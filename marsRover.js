3e799b1a1566271882bd688cdb5ff8186f425b1b
Jethet <henriette.hettinga@gmail.com>
2019-12-06 17:03:38 +0100

    Add input validation to main function; gives error

--- marsRover.js
+++ marsRover.js
@@ -80,56 +80,33 @@ function moveBackwards(rover){
 }
 function manageRover(rover, directions){
     for (let i = 0; i <= directions.length; i++){
-        let orientation = directions[i];
-        switch(orientation){
-            case "l":
-                turnLeft(rover, orientation);
-                //console.log(`Rover facing direction ${rover.direction}`);
-                break;
-            case "r":
-                turnRight(rover, orientation);
-                //console.log(`Rover facing direction ${rover.direction}`);
-                break;
-            case "f":
-                moveForward(rover, orientation);
-                console.log(`Rover facing direction ${rover.direction}, x is ${rover.x} and y is ${rover.y}`);
-                break;
-            case "b":
-                moveBackwards(rover, orientation);
-                console.log(`Rover facing direction ${rover.direction}, x is ${rover.x} and y is ${rover.y}`);
-                break;
+        if (directions[i] === "l" || directions[i] === "r" || directions[i] === "f" || directions[i] === "b"){
+            let orientation = directions[i];
+            switch(orientation){
+                case "l":
+                    turnLeft(rover, orientation);
+                    //console.log(`Rover facing direction ${rover.direction}`);
+                    break;
+                case "r":
+                    turnRight(rover, orientation);
+                    //console.log(`Rover facing direction ${rover.direction}`);
+                    break;
+                case "f":
+                    moveForward(rover, orientation);
+                    console.log(`Rover facing direction ${rover.direction}, x is ${rover.x} and y is ${rover.y}`);
+                    break;
+                case "b":
+                    moveBackwards(rover, orientation);
+                    console.log(`Rover facing direction ${rover.direction}, x is ${rover.x} and y is ${rover.y}`);
+                    break;
+            }
+        } else {
+            console.log("Please use correct directions: f, b, r and l.");
         }
     }
     rover.travelLog.push({x: rover.x, y: rover.y});
     console.log(rover.travelLog);
 }
 
-//TWO EXAMPLES:
 
-//manageRover(rover, "rffffffffffff");
-/* Result is:
-Rover facing direction E, x is 1 and y is 0
-Rover facing direction E, x is 2 and y is 0
-Rover facing direction E, x is 3 and y is 0
-Rover facing direction E, x is 4 and y is 0
-Rover facing direction E, x is 5 and y is 0
-Rover facing direction E, x is 6 and y is 0
-Rover facing direction E, x is 7 and y is 0
-Rover facing direction E, x is 8 and y is 0
-Rover facing direction E, x is 9 and y is 0
-Rover facing direction E, x is 10 and y is 0
-Rover facing direction E, x is 10 and y is 0
-Rover facing direction E, x is 10 and y is 0
-This means that a) changing direction works fine; b) rover stops moving forward when it reaches grid space 10
-But why is the message not printing that the rover moves out of the grid with the step over >9?
-*/
-
-//manageRover(rover, "f");
-/* Result is:
-Rover facing direction N, x is 0 and y is -1
-The rover should not move from 0 to -1: this should not be possible and the message "not outside the grid" should be printed.
-*/
-
-manageRover(rover, "bb");
-manageRover(rover, "rrb");
-manageRover(rover, "bbbb");
+manageRover(rover, "b");
