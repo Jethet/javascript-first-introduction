a2bf85f1938976f958fb367eff8045f40187a700
Jethet <henriette.hettinga@gmail.com>
2019-12-06 15:33:33 +0100

    Create travelLog property

--- marsForward.js
+++ marsForward.js
@@ -2,7 +2,8 @@
 const rover = {
     direction: "N",
     x: 0,
-    y: 0
+    y: 0,
+    travelLog: [{x:0, y:0}]
 };
 
 const grid = [
@@ -18,25 +19,22 @@ const grid = [
     [{x:9, y:0}, {x:9, y:1}, {x:9, y:2}, {x:9, y:3}, {x:9, y:4}, {x:9, y:5}, {x:9, y:6}, {x:9, y:7}, {x:9, y:8}, {x:9, y:9}],
  ];
 
-function moveForward(rover){
-    if (rover.x >= 0 && rover.x <= 9){
-        if (rover.y <= 0 && rover.y >= 9){
-            if (rover.direction === "N"){
-                rover.y--;
-                console.log(`Position is: ${rover.x}, ${rover.y}`);
-            } else if (rover.direction === "S"){
-                rover.y++;
-                console.log(`Position is: ${rover.x}, ${rover.y}`);
-            } else if (rover.direction === "W"){
-                rover.x--;
-                console.log(`Position is: ${rover.x}, ${rover.y}`);
-            }  else if (rover.direction === "E"){
-                rover.x++;
-                console.log(`Position is: ${rover.x}, ${rover.y}`);
-            } else {
-                console.log("The rover cannot be placed outside the grid.");
-        }
+ function moveForward(rover){
+    if (rover.x >= 0 && rover.x <= 9 && rover.y >= 0 && rover.y < 9){
+        if (rover.direction === "N"){
+            rover.y--;
+            //console.log(`Position is: ${rover.x}, ${rover.y}`);
+        } else if (rover.direction === "S"){
+            rover.y++;
+            //console.log(`Position is: ${rover.x}, ${rover.y}`);
+        } else if (rover.direction === "W"){
+            rover.x--;
+            //console.log(`Position is: ${rover.x}, ${rover.y}`);
+        } else if (rover.direction === "E"){
+            rover.x++;
+            //console.log(`Position is: ${rover.x}, ${rover.y}`);
+    } else {
+        console.log("The rover cannot be placed outside the grid.");
     }
     }
 }
-moveForward(rover);
