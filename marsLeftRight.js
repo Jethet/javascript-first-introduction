ca313ebbb7c717a8af3ca1c07f01407bc1e8d13f
Jethet <henriette.hettinga@gmail.com>
2019-12-06 15:18:02 +0100

    Update marsLeftRight functions

--- marsLeftRight.js
+++ marsLeftRight.js
@@ -1,39 +1,39 @@
 const rover = {
-    direction: "N"
+    direction: "N",
+    x: 0,
+    y: 0
 };
-
 
 function turnLeft(rover){
     if (rover.direction === "N"){
         rover.direction = "W";
-        console.log(rover.direction); 
+        //console.log(rover.direction); 
     } else if (rover.direction === "W"){
         rover.direction = "S";
-        console.log(rover.direction); 
+        //console.log(rover.direction); 
     } else if (rover.direction === "S"){
         rover.direction = "E";
-        console.log(rover.direction);
+        //console.log(rover.direction);
     } else if (rover.direction === "E"){
             rover.direction = "N";
-            console.log(rover.direction);
+            //console.log(rover.direction);
     }
 }
 
 function turnRight(rover){
     if (rover.direction === "N"){
         rover.direction = "E";
-        console.log(rover.direction); 
+        //console.log(rover.direction); 
     } else if (rover.direction === "E"){
         rover.direction = "S";
-        console.log(rover.direction); 
+        //console.log(rover.direction); 
     } else if (rover.direction === "S"){
         rover.direction = "W";
-        console.log(rover.direction);
+        //console.log(rover.direction);
     } else if (rover.direction === "W"){
             rover.direction = "N";
-            console.log(rover.direction);
+            //console.log(rover.direction);
     }
 }
-
 
 turnLeft(rover);
