cdb997445fd44a8abe5dd8ce0c4c4986d92b62dd
Jethet <henriette.hettinga@gmail.com>
2019-12-06 10:36:57 +0100

    Test with greater than/equal to operand

--- marsRover.js
+++ marsRover.js
@@ -38,8 +38,8 @@ function turnRight(rover){
 }
 
 function moveForward(rover){
-    if (rover.x >= 0 && rover.x < 9){
-        if (rover.y >= 0 && rover.y < 9){
+    if (rover.x >= 0 && rover.x <= 9){
+        if (rover.y >= 0 && rover.y <= 9){
             if (rover.direction === "N"){
                 rover.y--;
                 //console.log(`Position is: ${rover.x}, ${rover.y}`);
