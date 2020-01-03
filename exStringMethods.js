a7db758569f08c1085ba1c07ace213460ec82118
Jethet <henriette.hettinga@gmail.com>
2020-01-03 12:43:32 +0100

    Practice for Each

--- exStringMethods.js
+++ exStringMethods.js
@@ -16,19 +16,3 @@ let newArray = [0,1,2,3,4,5,6,7,8,9];
 newArray.splice(9,10);
 console.log(newArray); // [ 0, 3, 4, 5, 6, 7, 8, 9]
 
-
-for (let i=0; i<= newArray.length; i++){
-    console.log(i + 5);
-}
-
-newArray.forEach(function(i) {
-    console.log(i);
-});
-
-// THIS IS THE SAME AS arrow function:
-
-newArray.forEach(name => console.log(name));
-
-newArray.forEach(function(name){
-    console.log(`This is ${name}`);
-  });
