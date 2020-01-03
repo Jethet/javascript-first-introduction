3d8b80a1a852babdc1107321cf167df80a8487c4
Jethet <henriette.hettinga@gmail.com>
2020-01-03 14:10:15 +0100

    Testing string methods

--- exStringMethods.js
+++ exStringMethods.js
@@ -16,3 +16,16 @@ let newArray = [0,1,2,3,4,5,6,7,8,9];
 newArray.splice(9,10);
 console.log(newArray); // [ 0, 3, 4, 5, 6, 7, 8, 9]
 
+
+let mySentence = "I have no idea what to do with an arrow function and forEach with two parameters";
+console.log(mySentence.slice(7,11));
+let sepSentence = mySentence.split(" ");
+console.log(sepSentence);
+console.log(sepSentence.length);
+console.log(sepSentence[5]);
+console.log(sepSentence.indexOf("with"));
+console.log(sepSentence.includes("never"));
+console.log(sepSentence.slice(7,11));
+
+let testWord = "thisisatestword";
+console.log(testWord.slice(7,11));
