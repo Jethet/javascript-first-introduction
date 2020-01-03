8df50b0fd7264497121b9b01d6f801f51c1c2a7d
Jethet <henriette.hettinga@gmail.com>
2020-01-03 16:14:27 +0100

    Exercises on objects

---
+++ moreObjects.js
@@ -0,0 +1,14 @@
+let oneObject = {1:"One", 2:"Two", 3:"Three", 4:"Four", 5:"Five", 6:"Six", 7:"Seven",
+                 8:"Eight", 'nine':9, 'ten':10};
+let otherObject = oneObject;
+console.log(otherObject);
+
+console.log(oneObject[3]);
+console.log(otherObject.ten);
+
+oneObject.eleven = 11;
+console.log(oneObject);
+oneObject.twelve = 12;
+console.log(otherObject.nine);
+console.log('nine' in oneObject);
+
