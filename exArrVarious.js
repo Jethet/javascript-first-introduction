db6ea99c191e713930a430131343143898644cdc
Jethet <henriette.hettinga@gmail.com>
2020-01-03 15:23:14 +0100

    Exercises with array methods

--- exArrVarious.js
+++ exArrVarious.js
@@ -1,5 +1,9 @@
 // Count length of an array with .length: returns total index plus 1 (index starts with 0)
-let myArray = [1,2,3,4];
+let myArray = [1,2,3,4,5,6,7,8,9];
 console.log(myArray.length);
 
 console.log(myArray.every(item => item <9));
+console.log(myArray.filter(item => item % 2 === 0));
+console.log(myArray.forEach(item => item * 5)); // returns 'undefined' ??
+console.log(myArray.map(item => item * 10));
+console.log(myArray.some(item => item > 10));
