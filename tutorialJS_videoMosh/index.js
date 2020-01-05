dfcc3fcc18ccb05c280d8b526df830b703ded9ec
Jethet <henriette.hettinga@gmail.com>
2020-01-05 12:10:04 +0100

    Use selection in objects

--- tutorialJS_videoMosh/index.js
+++ tutorialJS_videoMosh/index.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+
+let person = {
+    name: 'John',
+    age: 25,
+    city: 'Barcelona'
+};
+
+console.log(person);
+console.log(person.name);
+
+person.name = 'Mary';
+console.log(person.name);
+
+let selection = 'name'
+person[selection] = 'Helen';
+console.log(person.name);
