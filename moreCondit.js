3930d555953b5e88ea75aeff36223369a6d2a0e5
Jethet <henriette.hettinga@gmail.com>
2020-01-05 14:32:27 +0100

    Use ternary operator

---
+++ moreCondit.js
@@ -0,0 +1,19 @@
+// TERNARY OPERATOR: ? stands for 'then' and : stands for 'else'
+
+const x = 10;
+
+const color = x > 10 ? 'red' : 'blue';
+console.log(color);
+
+switch(color) {
+    case 'red':
+        console.log('color is red');
+        break;
+    case 'blue':
+        console.log('color is blue');
+        break;
+    default:
+        console.log('color is not blue or red');
+        break;
+
+}
