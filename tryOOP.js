485618053a853f5d10dfc9b6aba7b3751a582b40
Jethet <henriette.hettinga@gmail.com>
2020-01-05 15:38:52 +0100

    Use constructor for date object to format date

--- tryOOP.js
+++ tryOOP.js
@@ -6,11 +6,13 @@
 function Person(firstName, lastName, dob) {
     this.firstName = firstName;
     this.lastName = lastName;
-    this.dob = dob;
+    this.dob = new Date(dob); // This is the constructor for a date
 }
 
 // instantiate object: these will be printed as 'Person', i.e. instance of Person object
-const person1 = new Person('Mary', 'Brown', '04-03-1969');
-const person2 = new Person('Helen', 'Davis', '14-08-1974');
-const person3 = new Person('Anna', 'Johnson', '29-11-1979');
+const person1 = new Person('Mary', 'Brown', '1969-04-03');
+const person2 = new Person('Helen', 'Davis', '1974-06-19');
+const person3 = new Person('Anna', 'Johnson', '1979-11-19');
+
+console.log(person1.dob, person2.dob);
 console.log(person1, person2, person3);
