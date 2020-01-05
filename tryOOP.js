7ae0f121262935a35ef2fc5fc5e75d544a9d2d6f
Jethet <henriette.hettinga@gmail.com>
2020-01-05 16:04:04 +0100

    Work with objects and prototypes for methods

--- tryOOP.js
+++ tryOOP.js
@@ -9,6 +9,17 @@ function Person(firstName, lastName, dob) {
     this.dob = new Date(dob); // This is the constructor for a date
 }
 
+// Using prototype with object name you can specify methods to be used with the object:
+Person.prototype.getBirthYear = function() {
+    return this.dob.getFullYear(); // Use the getFullYear() function on the dob propery
+};
+Person.prototype.getFullName = function(){
+    return `${this.firstName} ${this.lastName}`;
+};
+Person.prototype.charLastName = function(){
+    return `${this.firstName.length} is the length of ${this.firstName}`;
+};
+
 // instantiate object: these will be printed as 'Person', i.e. instance of Person object
 const person1 = new Person('Mary', 'Brown', '1969-04-03');
 const person2 = new Person('Helen', 'Davis', '1974-06-19');
@@ -16,3 +27,7 @@ const person3 = new Person('Anna', 'Johnson', '1979-11-19');
 
 console.log(person1.dob, person2.dob);
 console.log(person1, person2, person3);
+
+console.log(person1.getBirthYear());
+console.log(person1.getFullName());
+console.log(person2.charLastName());
