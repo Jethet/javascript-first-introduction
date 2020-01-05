9e679642d4b0ff874fe35e92164e2f190e5c6edc
Jethet <henriette.hettinga@gmail.com>
2020-01-05 14:15:17 +0100

    Work with array of objects and forEach, map and filter

--- objectsArray.js
+++ objectsArray.js
@@ -17,6 +17,39 @@ const todos = [
     }
 ];
 
+for (let i = 0; i < todos.length; i++){
+    console.log(todos[i].text);
+}
+
+for (let item of todos) {
+    console.log(item.id, item.text);
+}
+
+// forEach:
+todos.forEach(function(item) {
+    console.log(item.text, item.isCompleted);
+});
+
+// map ==>> RETURNS AN ARRAY, so you have to assign it to a variable:
+const idTodos = todos.map(function(item) {
+    return item.id;
+});
+console.log(idTodos);
+
+// filter:
+const completed = todos.filter(function(item) {
+    return item.isCompleted === true;
+});
+console.log(completed);
+
+// COMBINE filter, map:
+const completedTodos = todos.filter(function(item) {
+    return item.isCompleted === true;
+}).map(function(item){
+    return item.text;
+});
+console.log(completedTodos);
+
 /* Convert JS to JSON format: similar code:
 
 const todosJSON = JSON.stringify(todos);
