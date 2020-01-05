8514427c34100b639d8401f8fddb835de2ff6684
Jethet <henriette.hettinga@gmail.com>
2020-01-05 13:50:47 +0100

    Use JSON stringify

---
+++ objectsArray.js
@@ -0,0 +1,24 @@
+const todos = [
+    {
+        id: 1,
+        text: 'Backup laptop',
+        isCompleted: true
+    },
+
+    {
+        id: 2,
+        text: 'Meeting team',
+        isCompleted: true
+    },
+    {
+        id: 3,
+        text: 'Dentist appt',
+        isCompleted: false
+    }
+];
+
+/* Convert JS to JSON format: similar code:
+
+const todosJSON = JSON.stringify(todos);
+console.log(todosJSON);
+*/
