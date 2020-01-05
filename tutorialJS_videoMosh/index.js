f9b78057e06131998363530787240e040227aa98
Jethet <henriette.hettinga@gmail.com>
2020-01-05 13:35:02 +0100

    Destructuring arrays

--- tutorialJS_videoMosh/index.js
+++ tutorialJS_videoMosh/index.js
@@ -1,13 +1,28 @@
 
 let person = {
     name: 'John',
+    lastName: 'Doe',
     age: 25,
-    city: 'Barcelona'
+    hobbies: ['music', 'movies', 'sport'],
+    address: {
+        street: 'C/ Avila 20',
+        city: 'Barcelona',
+        country: 'Spain'
+    }
+    
 };
 
 console.log(person);
-console.log(person.name);
+console.log(person.name, person.lastName);
+console.log(person.hobbies[1]);
+console.log(person.address.city);
 
+const {name, lastName, address: {country}} = person;  // destructuring: pulling variables out of an object
+console.log(country);
+
+
+
+/*
 person.name = 'Mary';
 console.log(person.name);
 
@@ -19,3 +34,4 @@ console.log(person.name.split(''));
 let myArray = ['one', 'two', 'three', 'four'];
 console.log(Array.isArray(myArray));
 console.log(Array.isArray(person.name));
+*/
