6e4e7f512a7d4ce1b45180d8f9e84bcd8560e282
Jethet <henriette.hettinga@gmail.com>
2020-01-06 13:35:47 +0100

    Add explanation re purpose application

---
+++ JS_CrashCourse/main.js
@@ -0,0 +1,6 @@
+/* The tutorial explains how to create a very basic app where you can add
+ a user, grab the values, and then output the user. The <ul> items in the
+  .html file are commented out for this.
+*/
+
+// Grab elements from the DOM and add these to variables
