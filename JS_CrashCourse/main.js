7f326e6f7e74ad20fc2e27472d50bd22fc6710e4
Jethet <henriette.hettinga@gmail.com>
2020-01-06 12:42:23 +0100

    Work with the DOM

---
+++ JS_CrashCourse/main.js
@@ -0,0 +1,8 @@
+
+// Single element
+//console.log(document.getElementById('my-form'));
+//console.log(document.querySelector('h1'));
+
+
+// Multiple element
+console.log(document.querySelectorAll('.item'));
