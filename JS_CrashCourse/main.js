cd8f9e1cb036cb75f4bb507eb6a5f9798e32471f
Jethet <henriette.hettinga@gmail.com>
2020-01-06 12:59:16 +0100

    Using JS to change/delete/replace DOM items and text

--- JS_CrashCourse/main.js
+++ JS_CrashCourse/main.js
@@ -1,8 +1,13 @@
+// Manipulating the DOM
+// You can use JS methods to manipulate the DOM:
 
-// Single element
-//console.log(document.getElementById('my-form'));
-//console.log(document.querySelector('h1'));
+const items = document.querySelectorAll('.item'); // This takes the html list
+items.forEach((item) => console.log(item)); // This prints the items in the console
 
-
-// Multiple element
-console.log(document.querySelectorAll('.item'));
+const ul = document.querySelector('.items'); // Grab ul with class 'items'
+// ul.remove();  will remove the list of three items from the webpage
+// ul.lastElementChild.remove(); removes the third item from the list
+// ul.firstElementChild.textContent = 'Hello';  this changes the first list element
+                                              // text to 'Hello'
+// Grab second list element using the index and change text:
+ul.children[1].innerText = 'Whatever';
