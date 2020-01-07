b35aa4d80effdb86fa1d73d233fa4723ae3a4e81
Jethet <henriette.hettinga@gmail.com>
2020-01-07 12:55:39 +0100

    Sort numbers from low to high and vice versa

--- testOperator.js
+++ testOperator.js
@@ -1,3 +1,4 @@
+
 function moveForward(number){
     for (let i = 0; i < 9; i++){    
         console.log(i);
@@ -5,3 +6,11 @@ function moveForward(number){
 }
 
 moveForward(9);
+
+
+// Sort numbers from low to high: (other way: use y - x):
+numbers = [45, 98, 12,79, 35, 657, 233, 87];
+val = numbers.sort(function(x,y){
+    return x - y;
+});
+console.log(val);
